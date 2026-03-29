ecdc666245c153c0950e6b3748f2767e4190b860
Rukku <sardaarrukhnuddin@gmail.com>
2026-03-29 23:56:38 +0530

    Fix Vite asset filenames causing 404 on Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js?v=202403291057`,
-        chunkFileNames: `assets/[name].js?v=202403291057`,
-        assetFileNames: `assets/[name].[ext]?v=202403291057`
-      }
-    }
-  }
+  plugins: [react()]
 })
